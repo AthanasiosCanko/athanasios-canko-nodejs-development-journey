f3f58263fe8ce3bb6a32f9b04f7e22c5b75d1ffd
Athanasios Canko <cankoathanasios@gmail.com>
2020-09-01 23:04:26 +0200

    Implementing fs module to work with writeFileSync and appendFileSync methods

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,10 @@
-false.writeFileSync('notes.txt')
+const fs = require("fs")
+
+fs.appendFileSync('notes.txt', 'My name is Andrew.\n')
+
+//
+// Challenge: Append a file to notes.txt
+// 
+// 1. Use appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
