7080c6804de37184c46bbb6b82f2213996b6b3cd
Athanasios Canko <cankoathanasios@gmail.com>
2020-09-07 16:22:37 +0200

    Completing chalk challenge

---
+++ notes-app/chalk.js
@@ -0,0 +1,9 @@
+// Challenge: Use the chalk library in your project
+
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the string "Success!" to the console in green
+// 4. Test your work
+
+var chalk = require("chalk")
+console.log(chalk.green("Success!"))
